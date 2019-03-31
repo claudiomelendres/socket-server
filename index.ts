bc43874bd6d8b48a5d479d69f5aa2631beacfba3
claudio melendres <claudiomelendres@gmail.com>
2019-03-31 19:22:54 -0400

    Primer commit - rest server

---
+++ index.ts
@@ -0,0 +1,29 @@
+import Server from './classes/server'
+import router from './routes/router';
+
+import bodyParser from 'body-parser'
+import cors from 'cors'
+
+
+
+// const nombre = 'Claudio2';
+
+// console.log(`Mi nombre es ${ nombre }`);
+
+const server = new Server();
+
+// BodyParser
+server.app.use( bodyParser.urlencoded({ extended: true }));
+server.app.use( bodyParser.json());
+
+
+// CORS
+server.app.use( cors ({ origin:true, credentials: true}))
+
+
+// Rutas de servicios
+server.app.use('/', router)
+
+server.start( () => {
+    console.log(`Servidor corriendo en el puerto ${ server.port }`)
+})
